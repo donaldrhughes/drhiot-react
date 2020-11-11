ab822a8d8f35c4b94fad9332533eb5170adfc755
myrean <donaldrhughes@gmail.com>
2020-11-11 14:32:46 -0800

    cleanup context and add grid mat-ui

--- src/contexts/MainContext.js
+++ src/contexts/MainContext.js
@@ -1,78 +1,22 @@
 import React, { createContext, Component } from 'react';
-// import { isAuthenticated } from '../assets/js/exports'
 
 //Create the Main Context
 export const MainContext = createContext();
 
+//The "Context"
 class MainProvider extends Component {
-    //The "Context"
+
     state = {
-        // message: [],
-        // player: {},
-        // game: {},
-        // userObj: {},
-        // auth: isAuthenticated(),
         config: {},
-        // heldDice: [],
-        // inputArr: [],
-        // preConfig: {}
     }
     updateConfig = (config) => {
         this.setState({ config: config })
     }
-    // resetContext = () => {
-    //     this.setState({
-    //         message: [],
-    //         player: this.resetPlayer(),
-    //         game: {},
-    //         userObj: {},
-    //         auth: null,
-    //         config: {},
-    //         heldDice: [],
-    //         inputArr: [],
-    //         preConfig: {}
-    //     })
-    // }
-    // updateWinner = (winner) => {
-    //     this.setState({ winner: winner })
-    // }
-    // updatePreConfig = (preConfig) => {
-    //     this.setState({ preConfig: preConfig })
-    // }
-    // resetPlayer = () => {
-    //     this.setState({ player: {} })
-    // }
-    // updateUser = (userObj) => {
-    //     this.setState({ userObj: userObj })
-    // }
-    // updateHeldDice = (heldDice) => {
-    //     this.setState({ heldDice: heldDice })
-    // }
-    // updateInputArr = (inputArr) => {
-    //     this.setState({ inputArr: inputArr })
-    // }
- 
-    // updateMessage = (message) => {
-    //     this.setState({ message: message })
-    // };
-    // updateAuth = () => {
-    //     this.setState({ auth: isAuthenticated() })
-    // };
-    // updatePlayer = (player) => {
-    //     this.setState({ player: player })
-    // };
-    // updateGame = (game) => {
-    //     this.setState({ game: game })
-    // };
+
     render() {
         return (
             <MainContext.Provider value={{
-                ...this.state, 
-                // updateMessage: this.updateMessage,
-                // updateUser: this.updateUser, updatePlayer: this.updatePlayer, updateGame: this.updateGame, 
-                // resetContext: this.resetContext, updateAuth: this.updateAuth, updateHeldDice: this.updateHeldDice,
-                updateConfig: this.updateConfig, 
-                // updateInputArr: this.updateInputArr, updatePreConfig: this.updatePreConfig
+                ...this.state, updateConfig: this.updateConfig,
             }}>
                 {this.props.children}
             </MainContext.Provider>
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,35 +1,95 @@
-import React, { Component } from "react";
+import React, { useContext } from 'react';
 import "../../App.scss";
 import "./home.scss";
 
 //Components
-import { Grid } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
 
 //Contexts
 import { MainContext } from '../../contexts/MainContext'
 
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  paper: {
+    padding: theme.spacing(1),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+  },
+}));
 
-export default class Home extends Component {
-    static contextType = MainContext;
+export default function Home() {
+  const classes = useStyles();
+  const MC = useContext(MainContext);
 
-    render() {
-        return (
-            <Grid container>
-                <Grid item xs={4} style={bgStyle}>
-                    1
+  function FormRow() {
+    return (
+      <React.Fragment>
+        <Grid item xs={4}>
+          <Paper className={classes.paper}>Logo</Paper>
         </Grid>
-                <Grid item xs={4} style={bgStyle}>
-                    2
+        <Grid item xs={4}>
+          <Paper className={classes.paper}>NAV</Paper>
         </Grid>
-                <Grid item xs={4} style={bgStyle}>
-                    3
+        <Grid item xs={4}>
+          <Paper className={classes.paper}>Body</Paper>
         </Grid>
-            </Grid>
-        );
-    }
+      </React.Fragment>
+    );
+  }
+
+  return (
+    <div className={classes.root}>
+      <Grid container spacing={1}>
+        <Grid container item xs={12} spacing={3}>
+          <FormRow />
+        </Grid>
+        <Grid container item xs={12} spacing={3}>
+          <FormRow />
+        </Grid>
+        <Grid container item xs={12} spacing={3}>
+          <FormRow />
+        </Grid>
+      </Grid>
+    </div>
+  );
 }
 
-const bgStyle = {
-    background: "grey",
-    textAlign: "center"
-}
+// import React, { Component } from "react";
+
+
+// 
+// import { Grid } from '@material-ui/core';
+// import { Paper } from '@material-ui/core';
+// import { makeStyles } from '@material-ui/core/styles';
+
+// 
+
+
+// export default class Home extends Component {
+//     static contextType = MainContext;
+
+//     render() {
+//         return (
+//             <Grid container spacing={1}>
+//                 <Grid item xs={4} style={bgStyle}>
+//                     1
+//         </Grid>
+//                 <Grid item xs={4} style={bgStyle}>
+//                     2
+//         </Grid>
+//                 <Grid item xs={4} style={bgStyle}>
+//                     3
+//         </Grid>
+//             </Grid>
+//         );
+//     }
+// }
+
+// const bgStyle = {
+//     background: "grey",
+//     textAlign: "center"
+// }
